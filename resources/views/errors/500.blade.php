dd0d2214ad8b02a8d79da428815d5c99db1a884f
Mahdi Masaeli <masaelimahdiwork@gmail.com>
2024-03-17 14:57:49 +0330

    add logic code for error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css'])
+    @vite(['resources/css/fontiran.css'])
+    <title>404 Error</title>
+</head>
+<body>
+    <main class="flex justify-center items-center flex-col min-h-screen">
+        <div class="w-[90%] pc:w-[50%] mx-auto">
+            <img class="w-full" src="{{ Vite::asset('resources/assets/error page/500.svg') }}" alt="404 error">
+        </div>
+        <div class="text-center iransans-bold">
+            <a class="bg-[#1c4e00] p-2 tablet:p-3 rounded-md text-white text-xs tablet:text-base pc:text-lg" href="{{ env('APP_URL')}}">بازگشت به خانه</a>
+        </div>
+    </main>
+</body>
+</html>
