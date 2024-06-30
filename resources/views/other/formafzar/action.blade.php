f1b4d56cece28d874d1714d66a93b5965bc141bc
Mahdi Masaeli <masaelimahdiwork@gmail.com>
2024-07-01 01:15:12 +0330

    Feat: add temp pages;

---
+++ resources/views/other/formafzar/action.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <link rel="shortcut icon" href="/life-cycle.png" type="image/x-icon">
+    <title>ثبت فراخوان عملیات</title>
+</head>
+
+<body>
+    <div class="w-full min-h-screen">
+        <iframe class="w-full min-h-screen" src="https://formafzar.com/form/y6iml" height="100%"></iframe>
+    </div>
+
+</body>
+
+</html>
